aea85a8ed7ca09b9b65b20247521f8633a569154
Baby_outcast <prasanthchettri3@gmail.com>
2020-10-07 10:40:48 +0530

    Added Database Models

--- index/models.py
+++ index/models.py
@@ -1,19 +1,45 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
+#TODO : make more robust and scalable
 class UserModel(models.Model):
     alias = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 10)
     bio =  models.CharField(max_length = 600)
+    USERNAME_FEILD = 'alias'
+    REQUIRED_FEILDS = ['alias' , 'bio']
+    def __str__(self):
+    	return str(self.alias)
+
 
 class PostModel(models.Model):
     User = models.ForeignKey(to = UserModel ,on_delete = models.CASCADE , default = 1 , related_name = "PostModel")
     writeup = models.CharField(max_length = 600)
-    validations = models.IntegerField(default = 1)
     date_c = models.DateTimeField(default = timezone.now)
+    def __str__(self):
+    	return str(self.writeup)
 
 class CommentModel(models.Model):
-    comment_to = models.ForeignKey(to = PostModel , on_delete = models.CASCADE , related_name='comments')
-    comment_dat = models.CharField(max_length = 100)
-    def __str__(self):
-        return str(self.comment_dat)
+	comment_by = models.ForeignKey(to = UserModel , on_delete = models.CASCADE , related_name='comment_by' , default = None)
+	comment_to = models.ForeignKey(to = PostModel , on_delete = models.CASCADE , related_name='comment_to' , default = None)
+	comment_dat = models.CharField(max_length = 100)
+	def __str__(self):
+		return str(self.comment_dat)
+
+class validations(models.Model):
+	#user that Validated
+    user = models.ForeignKey(to = UserModel,  on_delete = models.CASCADE , related_name = "User")
+	#submissions that were validated
+    submission = models.ForeignKey(to = PostModel,  on_delete = models.CASCADE , related_name = "submission")
+
+    @classmethod
+    def validate(submission : PostModel ,  user : UserModel):
+    	is_validated = validations.objects.filter(user = user , submission = submission)
+    	#if zero object not there so make else object there so delete
+    	if is_validated.exists() : 
+    		is_validated[0].delete()
+    	else :
+    		new_validate = validate(user = user , submission = submission)
+    		new_validate.save()
--- index/views.py
+++ index/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import PostModel
 
 def frontpage(request):
-	return render(request ,"home.html")
+    return render(request , "home.html" , context = {'posts' : PostModel.objects.all().order_by('-date_c')})
 
 def getNew(request):
 	return render(request , 'new.html')
