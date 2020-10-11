bfdfc9ea82a0b41970c05c1b0054b824ca8605e8
Baby_outcast <prasanthchettri3@gmail.com>
2020-10-11 21:09:47 +0530

    Started building the feed Lots of good ideas can be implemented hopefully I don't get bogged down by bugs

--- index/views.py
+++ index/views.py
@@ -6,8 +6,11 @@ from django.urls import reverse
 def frontpage(request):
     return render(request , "index/home.html" , context = {'posts' : PostModel.objects.all().order_by('-date_c')})
 
-def getNew(request):
-	return render(request , 'index/new.html')
-	
+def TopLiteratureView(request):
+    return render(request , "index/home.html" , context = {'posts' : PostModel.objects.all().order_by('-date_c')})
+
+def updatevalidate(request):
+	return render(request ,'index/starred.html')
+
 def getStarred(request):
 	return render(request ,'index/starred.html')
