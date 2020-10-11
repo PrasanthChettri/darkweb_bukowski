a65085afd242d355d70b4f75ba345b5d405a1c73
Baby_outcast <prasanthchettri3@gmail.com>
2020-10-11 22:59:05 +0530

    Dealing with Validations/likes functionality

--- index/views.py
+++ index/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect 
-from .models import PostModel
+from .models import PostModel , validations
 from django.urls import reverse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def frontpage(request):
     return render(request , "index/home.html" , context = {'posts' : PostModel.objects.all().order_by('-date_c')})
@@ -9,8 +11,21 @@ def frontpage(request):
 def TopLiteratureView(request):
     return render(request , "index/home.html" , context = {'posts' : PostModel.objects.all().order_by('-date_c')})
 
+@csrf_exempt
 def updatevalidate(request):
-	return render(request ,'index/starred.html')
+	if request.method == "POST":
+		post_id = request.POST.get('post_id')
+		post = PostModel(id = post_id)
+		is_liked =  False 
+		if validation_instance := validations.objects.filter(user= request.user , submission = post):
+			validation_instance.delete()
+		else : 
+			validation_instance = validations(user = request.user , submission = post)
+			validation_instance.save()
+			is_liked  = True 
+		return JsonResponse({'liked' : is_liked})
+	return HttpResponse("Post stuff don't try to be a hacker")
+
 
 def getStarred(request):
 	return render(request ,'index/starred.html')
