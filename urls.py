dc3684bb74c01482022cf1f76ff5b07fb3dc5813
Baby_outcast <prasanthchettri3@gmail.com>
2020-10-16 12:47:41 +0530

    THIS

--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path  , include
 
+#URL FOR BOTH APPS ACCOUNTED FOR
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
